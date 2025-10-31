fedc1dbe06a085ff9912702fbc66c6737752b7f4
Matouš Čmelík <cmeldas@gmail.com>
2025-10-31 21:01:14 +0100

    Add Czech and English translations for product label fields and update field names in models and views

--- models/product.py
+++ models/product.py
@@ -5,7 +5,7 @@ class ProductProduct(models.Model):
 
     label_title = fields.Char(string='Title', copy=True)
     label_subtitle = fields.Char(string='Subtitle', copy=True)
-    label_ingredient = fields.Text(string='Ingredients', copy=True)
+    label_ingredients = fields.Text(string='Ingredients', copy=True)
     label_lifetime = fields.Integer(string='Lifetime (days)', copy=True)
     label_storage_temp = fields.Integer(string='Storage Temperature (°C)', copy=True, default=8 )
     label_fixed_weight = fields.Char(string='Fixed weight', copy=True, default=False, help="Leave empty to use scale weight during printing labels")
